94302fa4963787c0dddb25db96e4cbcdbed0da90
AnanTripathi046 <2018ugee052@nitjsr.ac.in>
2020-02-13 19:06:20 +0530

    Commit after crating all the files

---
+++ app/src/main/java/com/example/fragmentproject/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.fragmentproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
