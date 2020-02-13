2fd71b3a6def2514ca7024dcabe743b7bb35f1d9
AnanTripathi <aman.tripathi046@gmail.com>
2020-02-14 00:03:05 +0530

    final commit

--- app/src/main/java/com/example/fragmentproject/MainActivity.java
+++ app/src/main/java/com/example/fragmentproject/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.fragmentproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    SectionStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starts");
+        mSectionsStatePagerAdapter =new SectionStatePagerAdapter(getSupportFragmentManager());
+        mViewPager=findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+    }
+
+    private void setupViewPager(ViewPager mViewPager) {
+        SectionStatePagerAdapter adapter= new SectionStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment1(),"Fragment1");
+        adapter.addFragment(new Fragment2(),"Fragment2");
+        adapter.addFragment(new Fragment2(),"Fragment2");
+        mViewPager.setAdapter(adapter);
+    }
+    public void setViewPager(int fragmentNumber)
+    {
+        mViewPager.setCurrentItem(fragmentNumber);
     }
 }
